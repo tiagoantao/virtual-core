112b7cc52717c5810c971a5035394454c95811bb
Tiago Antao <tra@popgen.net>
2016-02-29 09:13:17 -0700

    copy in-VOLUME configuration files

--- src/create_directory_structure.py
+++ src/create_directory_structure.py
@@ -1,4 +1,6 @@
 import glob
+import os
+import shutil
 import sys
 
 import yaml
@@ -9,9 +11,26 @@ if len(sys.argv) != 2:
 else:
     my_dir = sys.argv[1]
 
-replace = '/path_to_'
+
+def make_dir(my_dir):
+    try:
+        os.mkdir(my_dir)
+    except FileExistsError:
+        pass  # This is OK
 
 samples = glob.glob('ansible/roles/**/vars/main.yml')
 for sample in samples:
     conf = yaml.load(open(sample))
-    print(conf)
+    for k, container_dir in conf.items():
+        if not k.endswith('_dir'):
+            continue
+        machine = k[:-4]
+        make_dir(my_dir + machine)
+        for sample_dir, dirs, fnames in os.walk('docker/%s/conf' % machine):
+            root = '/'.join([my_dir, machine] + sample_dir.split('/')[3:])
+            for in_dir in dirs:
+                make_dir(root +  '/' + in_dir)
+            for fname in fnames:
+                if fname.endswith('.sample'):
+                    continue
+                shutil.copyfile(sample_dir + '/' + fname, root)
