6a0f1c235ee50633e392a9b8998c9c3ad24466a5
Tiago Antao <tra@popgen.net>
2016-03-09 09:20:31 -0700

    software list

--- docker/software/install.sh
+++ docker/software/install.sh
@@ -6,10 +6,10 @@ bash Miniconda3-latest-Linux-x86_64.sh -p /software/conda -b
 /software/conda/bin/conda config --add channels r
 /software/conda/bin/conda config --add channels bioconda
 /software/conda/bin/conda install -y bwa htslib samtools vcftools bowtie2
-/software/conda/bin/conda install -y blast fastqc fastx_toolkit 
-/software/conda/bin/conda install -y trimmomatic picard genepop bcftools 
-/software/conda/bin/conda install -y bedtools vcflib freebayes
-/software/conda/bin/conda install -y cutadapt mafft kraken mothur mummer
+/software/conda/bin/conda install -y blast fastqc fastx_toolkit seqtk rainbow
+/software/conda/bin/conda install -y trimmomatic picard genepop bcftools bayescan
+/software/conda/bin/conda install -y bedtools vcflib freebayes stacks pear
+/software/conda/bin/conda install -y cutadapt mafft kraken mothur mummer cd-hit
 /software/conda/bin/conda install -y muscle novoalign picard raxml snpeff spades trinity
 
 /software/conda/bin/conda install -y bioconductor-biocinstaller
@@ -17,3 +17,7 @@ bash Miniconda3-latest-Linux-x86_64.sh -p /software/conda -b
 /software/conda/bin/conda install -y jupyter scipy scikit-learn matplotlib
 /software/conda/bin/conda install -y biopython
 
+/software/conda/bin/conda create -n python2 python=2
+source /software/conda/bin/conda/activate python2
+/software/conda/bin/conda install -c bioconda pyrad
+source /software/conda/bin/conda/deactivate
