10fed3672262f792a5f9fe8ee56416255b135db2
Tiago Antao <tra@popgen.net>
2016-04-05 15:49:44 -0600

    user server

---
+++ docker/user-server/add_software.sh
@@ -0,0 +1,7 @@
+apt-get upgrade -y
+
+apt-get install -y git vim make gcc tmux
+apt-get install -y python-dev
+
+apt-get install -y build-essential cmake zlib1g-dev libbz2-dev nano screen 
+apt-get install -y htop
