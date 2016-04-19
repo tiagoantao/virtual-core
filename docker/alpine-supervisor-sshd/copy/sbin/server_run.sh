20bf065db2022bde194e69eb9ea4439650bf1262
Tiago Antao <tra@popgen.net>
2016-04-19 11:59:13 -0600

    magic bug on file name

--- docker/alpine-supervisor-sshd/copy/sbin/server_run.sh
+++ docker/alpine-supervisor-sshd/copy/sbin/server_run.sh
@@ -2,9 +2,9 @@
 
 /sbin/link_mutable_files.py
 
-if [ -e /sbin/prepare-magic.sh ]
+if [ -e /sbin/prepare_magic.sh ]
 then
-    bash /sbin/prepare-magic.sh
+    bash /sbin/prepare_magic.sh
 fi
 
 /usr/bin/supervisord
