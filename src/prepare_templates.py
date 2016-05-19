34ec2bd0b46fc911dc8a6d86106b667e20252bf7
Tiago Antao <tiagoantao@gmail.com>
2016-05-19 09:09:38 -0600

    support for list in ansible configuration files

--- src/prepare_templates.py
+++ src/prepare_templates.py
@@ -15,14 +15,22 @@ if len(sys.argv) != 2:
 else:
     my_dir = sys.argv[1]
 
-replace = '/path_to_'
+replace_str = '/path_to_'
+
+
+def replace(obj, pattern, replacement):
+    if type(obj) == str:
+        return obj.replace(pattern, replacement)
+    elif type(obj) == list:
+        return [e.replace(pattern, replacement) for e in obj]
+    raise Exception()
 
 samples = glob.glob('ansible/roles/**/vars/main.yml.sample')
 for sample in samples:
     root = sample[:-7]
     print(root)
     conf = yaml.load(open(sample))
-    new_conf = {k: v.replace(replace, my_dir) for k, v in conf.items()}
+    new_conf = {k: replace(v, replace_str, my_dir) for k, v in conf.items()}
     w = open(root, 'wt', encoding='utf-8')
     w.write(yaml.dump(new_conf, default_flow_style=False))
     w.close()
