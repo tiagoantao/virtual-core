bfc2d63a7a9400196096b492e4eae5db0d771c2c
Tiago Antao <tiagoantao@gmail.com>
2016-05-23 15:27:59 -0600

    create supervisor log directory

--- docker/ubuntu-supervisor-sshd/copy/sbin/server_run.sh
+++ docker/ubuntu-supervisor-sshd/copy/sbin/server_run.sh
@@ -7,4 +7,9 @@ then
     bash /sbin/prepare_magic.sh
 fi
 
+#in the (probable) case /var/log is a VOLUME
+if [[ ! -e /var/log/supervisor ]]; then
+   mkdir /var/log/supervisor
+fi
+
 /usr/bin/supervisord
