8dda36c9e8e427cae835f28e30b64dd0b692b774
Tiago Antao <tra@popgen.net>
2016-08-01 10:29:29 -0600

    assorted software for installation

--- docker/software/install.sh
+++ docker/software/install.sh
@@ -3,35 +3,40 @@
 wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 bash Miniconda3-latest-Linux-x86_64.sh -p /software/conda -b
+CONDA=/software/conda/bin/conda
 
-/software/conda/bin/conda config --add channels r
-/software/conda/bin/conda config --add channels bioconda
+$CONDA config --add channels r
+$CONDA config --add channels bioconda
+$CONDA config --add channels conda-forge
 
-/software/conda/bin/conda install -y conda-build
+$CONDA install -y conda-build
+$CONDA install -y pytables blosc dask pyqt ipyparallel
+$CONDA install -y r-irkernel pcre
+$CONDA install -y r-ggplot2 r-plyr r-stringi r-igraph r-cpp r-quadprog
 
-/software/conda/bin/conda install -y bwa htslib samtools vcftools bowtie2
-/software/conda/bin/conda install -y blast fastqc fastx_toolkit seqtk rainbow
-/software/conda/bin/conda install -y trimmomatic picard genepop bcftools bayescan
-/software/conda/bin/conda install -y bedtools vcflib freebayes stacks pear qualimap
-/software/conda/bin/conda install -y cutadapt mafft kraken mothur mummer cd-hit
-/software/conda/bin/conda install -y muscle novoalign picard raxml snpeff spades trinity
-/software/conda/bin/conda install -y ddocent eigensoft
+$CONDA install -y bwa htslib samtools vcftools bowtie2
+$CONDA install -y blast fastqc fastx_toolkit seqtk rainbow
+$CONDA install -y plink2
+$CONDA install -y trimmomatic picard genepop bcftools bayescan
+$CONDA install -y bedtools vcflib freebayes stacks pear qualimap
+$CONDA install -y cutadapt mafft kraken mothur mummer cd-hit
+$CONDA install -y muscle novoalign picard raxml snpeff spades trinity
+$CONDA install -y fastq-join pear
+$CONDA install -y eigensoft pyvcf pysam
 
-/software/conda/bin/conda install -y bioconductor-biocinstaller
+$CONDA install -y bioconductor-biocinstaller
+$CONDA install -y bioconductor-edger
 
-/software/conda/bin/conda install -y jupyter scipy scikit-learn matplotlib
-/software/conda/bin/conda install -y biopython seaborn pandas
+$CONDA install -y jupyter jupyterhub scipy scikit-learn matplotlib
+$CONDA install -y seaborn pandas rpy2
+$CONDA install -y biopython
 
-/software/conda/bin/conda create -n dDocent python=3
-source /software/conda/bin/conda/activate dDocent
-/software/conda/bin/conda install -c bioconda ddocent
-source /software/conda/bin/conda/deactivate
+$CONDA create -n python2 python=2
+source $CONDA/activate python2
+$CONDA install -y pyrad 
+$CONDA install -y fastq-join pear
 
-/software/conda/bin/conda create -n python2 python=2
-source /software/conda/bin/conda/activate python2
-/software/conda/bin/conda install -c bioconda pyrad 
-/software/conda/bin/conda install -y jupyter scipy scikit-learn matplotlib
-/software/conda/bin/conda install -y biopython seaborn pandas
-/software/conda/bin/conda install -c http://conda.binstar.org/bpeng simuPOP
+$CONDA install -c http://conda.binstar.org/bpeng simuPOP
+$CONDA install -y biopython qiime qiime-default-reference
 
-source /software/conda/bin/conda/deactivate
+source $CONDA/deactivate
