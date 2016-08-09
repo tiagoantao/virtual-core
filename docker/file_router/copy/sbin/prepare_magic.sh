a86476efe58319070783c4895a752e0049d9d10c
Tiago Antao <tra@popgen.net>
2016-08-09 12:05:54 -0600

    file server: samba support

--- docker/file_router/copy/sbin/prepare_magic.sh
+++ docker/file_router/copy/sbin/prepare_magic.sh
@@ -4,4 +4,4 @@ while read name id passwd; do
     useradd "$name" -M -u "$id"
     printf "${passwd}\n${passwd}\n" | pdbedit -a -u "$name" -t 
 done < <(cut -f1,2,3 --output-delimiter=' ' /etc/samba/users)
-mv /etc/samba/users /etc/samba/users.done
+#mv /etc/samba/users /etc/samba/users.done
