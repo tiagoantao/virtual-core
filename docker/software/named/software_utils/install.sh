8ee2f2a1690b8a5cc7959dca53b7e6b3d4de0986
Tiago Antao <tra@popgen.net>
2017-01-11 08:41:35 -0700

    upgraded list of software

--- docker/software/named/software_utils/install.sh
+++ docker/software/named/software_utils/install.sh
@@ -12,19 +12,21 @@ $CONDA config --add channels r
 $CONDA config --add channels bioconda
 $CONDA config --add channels conda-forge
 
-$CONDA install -y conda-build
+$CONDA install -y conda-build gcc
 $CONDA install -y pytables blosc dask pyqt ipyparallel
 $CONDA install -y r-irkernel pcre
-$CONDA install -y r-ggplot2 r-plyr r-stringi r-igraph r-cpp r-quadprog
+$CONDA install -y r-ggplot2 r-plyr r-stringi r-igraph r-rcpp r-quadprog
+$CONDA install -y r-sp pydotplus
+$CONDA install -y tensorflow
 
 $CONDA install -y bwa htslib samtools vcftools bowtie2
 $CONDA install -y blast fastqc fastx_toolkit seqtk rainbow
-$CONDA install -y plink2
+$CONDA install -y plink2 hmmer
 $CONDA install -y trimmomatic picard genepop bcftools bayescan
 $CONDA install -y bedtools vcflib freebayes stacks pear qualimap
 $CONDA install -y cutadapt mafft kraken mothur mummer cd-hit
 $CONDA install -y muscle novoalign picard raxml snpeff spades trinity
-$CONDA install -y fastq-join pear
+$CONDA install -y fastq-join pear gffutils
 $CONDA install -y eigensoft pyvcf pysam
 
 $CONDA install -y bioconductor-biocinstaller
@@ -32,15 +34,18 @@ $CONDA install -y bioconductor-edger
 
 $CONDA install -y jupyter jupyterhub scipy scikit-learn matplotlib
 $CONDA install -y seaborn pandas rpy2
-$CONDA install -y biopython
-$CONDA install -y bioblend
+$CONDA install -y biopython kallisto
+$CONDA install -y bioblend blat
+$CONDA install -y stacks transdecoder
 
-$CONDA create -n python2 python=2
+$CONDA create -y -n python2 python=2
 source $CONDA/activate python2
 $CONDA install -y pyrad 
 $CONDA install -y fastq-join pear
 
-$CONDA install -c http://conda.binstar.org/bpeng simuPOP
+$CONDA install -y -c http://conda.binstar.org/bpeng simuPOP
 $CONDA install -y biopython qiime qiime-default-reference bioblend
 
 source $CONDA/deactivate
+
+$CONDA create -y -n trinity trinity
